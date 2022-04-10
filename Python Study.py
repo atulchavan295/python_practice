52c8e1b956f105efe922238644c2653f474f24cc
atulchavan295 <102897144+atulchavan295@users.noreply.github.com>
2022-04-10 13:51:40 +0530

    Add files via upload

---
+++ Python Study.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[24]:
+
+
+
+
+
+# In[38]:
+
+
+a = "As an Information Security Professional"
+b = "I have a good knowledge in understanding the architecture, design,management and controls that assures the security of business environments and incident handling through log analysis"
+c = "Identifying the root causes of security incidents and collecting useful data for compliance reporting"
+d = "I have experience on Security Information and Event Management (SIEM), as a result I am familiar with the SIEM tool HP's ArcSight, RSA Security Analytics and IBM’s QRadar."
+e = "Integrating various security devices to the SIEM setup for log management and Analysis"
+f = "As a part of SOC team, I have worked on Symantec AV, Symantec web gateway and Symantec mail gateway"
+g = "My prime work includes Security Log Monitoring and Analysis from various organizational network devices for Indian and foreign customers."
+h = "I have a good log reading capability for devices such as IPS/IDS, Anti-virus, different Firewalls like ASA, heckpoint, Operating Systems like Windows, Linux etc."
+
+
+print (a, end=",")
+print (b+".",end="\n\n")
+print (c, end=".")
+print (d, end="")
+print (e+".", end="\n\n")
+print (f, end=".")
+print (g, end="")
+print (h)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
